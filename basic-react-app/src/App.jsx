2febcc9afefbe0b235a339ac5d7ce53adcdaf4f3
Ankit Varshney <akvrsny@gmail.com>
2026-07-09 00:21:57 +0530

    Add basic React app

---
+++ basic-react-app/src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Title from "./Title.jsx";
+import ProductTab from './ProductTab.jsx';
+
+
+function App() {
+  return (
+    <>
+      <ProductTab/>
+      <h1>this is my app component </h1>
+      <p>inside app component we have :</p>
+      <Title/>
+      <Title/>
+      <Title/>
+      <Title/>
+
+    </>
+  );
+}
+
+export default App;
+
+
---
+++ basic-react-app/src/Title.jsx
@@ -0,0 +1,18 @@
+// function Title() {
+//     return <h1>I am the Title</h1>;
+// }
+
+function Title() {
+    let name = "shradha";
+    return (
+        <div>
+            <p>2*2 = {2*2}</p>
+            <p>Hi, {name.toUpperCase()}</p>
+        </div>
+
+    );
+}
+
+
+export default Title;
+// export Title;
