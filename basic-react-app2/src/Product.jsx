c9ea33d28e2d9b82845ee88a98151a31e92bd728
Ankit Varshney <akvrsny@gmail.com>
2026-07-10 09:25:10 +0530

    Add React module 2 practice project

---
+++ basic-react-app2/src/Product.jsx
@@ -0,0 +1,12 @@
+import "./Product.css";
+
+function Product() {
+    return  (
+        <div className = "Product">
+           
+            <h5>Product Description</h5>
+        </div>
+    );
+}
+
+export default Product;
