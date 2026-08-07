faf3fc1d06a9a37645834a7a067316fffbd378a7
Ankit Varshney <akvrsny@gmail.com>
2026-08-07 23:52:31 +0530

    express

--- Backend_Express/index.js
+++ Backend_Express/index.js
@@ -3,21 +3,15 @@ const app = express();
 
 console.dir(app);
 
+let port = 3000;
 
+app.listen(port, () => {
+    console.log(`app is listening on port ${port}`);
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use((req,res) => {
+    console.log("request received");
+});
 
 
 
