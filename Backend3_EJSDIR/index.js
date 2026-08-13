655d1e73f251eb7d48bdb92ca65de8661357fb68
Ankit Varshney <akvrsny@gmail.com>
2026-08-13 23:38:06 +0530

    EJS

--- Backend3_EJSDIR/index.js
+++ Backend3_EJSDIR/index.js
@@ -3,12 +3,19 @@ const app = express();
 
 const port = 8080;
 
-app.listen(port, ()=>{
-    console.log(`listening on port ${port}`);
+app.set("view engine", "ejs");
+
+app.get("/",(req,res) => {
+    
+    res.render("home.ejs");
 
 });
 
+app.listen(port, ()=>{
 
+    console.log(`listening on port ${port}`);
+
+});
 
 
 
