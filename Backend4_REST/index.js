2ba38b3a8cc9af94614671a673b6ca25f35e1822
Ankit Varshney <akvrsny@gmail.com>
2026-08-26 03:18:33 +0530

    rest

--- Backend4_REST/index.js
+++ Backend4_REST/index.js
@@ -3,9 +3,12 @@ const app = express();
 const port = 8080;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
+
 uuidv4();  
 
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -67,9 +70,11 @@ app.get("/posts/:id",(req,res) => {
     
 });
 
-app.get("/posts/new",(req,res) => {
+app.get("/posts/:id/edit",(req,res) => {
 
-    res.render("edit.ejs");
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs" ,{ post });
 
 });
 
@@ -82,11 +87,17 @@ app.patch("/posts/:id",(req,res) =>
     post.content = newContent;
     console.log(post);
     console.log(id);
-    res.send("patch request working");
+    res.redirect("/posts");
 
 });
 
+app.delete("/posts/:id",(req,res)=> {
 
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+
+});
 
 
 app.listen(port, () => {
