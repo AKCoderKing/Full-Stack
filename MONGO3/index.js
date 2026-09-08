2469933bf7f3d732476ff52a871910f3eeaab0c5
Ankit Varshney <akvrsny@gmail.com>
2026-09-09 00:01:07 +0530

    mongo

---
+++ MONGO3/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+const Chat = require("./models/chat.js");
+
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public"))); // forusing the style.css file we use this
+app.use(express.urlencoded({extended: true}));
+
+
+main().then( () => {   // establishment of connection
+    console.log("connection successful");
+}) 
+.catch(err => console.log(err));
+
+let chat1 = new Chat({
+    from: "neha",
+    to: "priya",
+    msg: "send me your exam sheets",
+    created_at: new Date()
+});
+
+chat1.save().then((res) => {
+    console.log(res);
+});
+
+
+async function main() {
+   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+
+
+// Index Route
+app.get("/chats", async (req,res) => {
+    let chats = await Chat.find() // is mai hum koi condition nhi denge kyuki huko joh h sabhi   chats chahiye 
+    console.log(chats);
+    res.render("index.ejs",{chats});
+});
+
+// New Route
+app.get("/chats/new", (req,res)=> {
+    res.render("new.ejs");
+});
+
+//Create Route
+app.post("/chats",(req,res) => {
+   let {from,to,msg} = req.body;
+   let newChat = new Chat({
+    from: from,
+    to: to,
+    msg: msg,
+    created_at: new Date()
+   });
+   newChat
+      .save()
+      .then((res) => {
+        console.log("chat was saved");
+      })  
+      .catch((err) => {
+        console.log(err);
+      });
+
+   res.redirect("/chats");
+
+});
+
+//edit Route
+app.get("/chats/:id/edit", async(req,res) => {
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs", { chat });
+});
+
+app.get("/",(req,res) => {
+    res.send("root is working");
+});
+
+app.listen(8080, () => {
+    console.log("server is listening on port 8080");
+    
+});
+
+
+
+
