6f3de4bca25b8b6fa6be1b38f44ef4efce2d0161
Ankit Varshney <akvrsny@gmail.com>
2026-09-17 23:37:17 +0530

    middleware

---
+++ MIDDLEWARES/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+
+// app.use((req,res,next) => {
+//     let {query} = req.query;
+//     console.log(query);
+//     console.log("Hi , I am middleware");
+//     next();
+// });
+
+// app.use((req,res,next) => {
+
+//     console.log("Hi , I am 2nd middleware");
+//     next();
+// });
+
+app.use((req,res,next) => {
+    res.time = Date.now();
+    console.log(req.method,req.hostname,req.path,req.time);
+    next();
+});
+
+app.get("/", (req,res) => {
+    res.send("Hi, I am root");
+});
+
+app.get("/random" , (req,res) =>{
+    res.send("this is a random page");
+});
+
+app.listen(8080, () => {
+    console.log("server listening to port 8080");
+});
+
+
